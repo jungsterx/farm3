81e8c3ffb28e12396902a40f0f860e106026986d
jungsterx <jialeng.zheng@gmail.com>
2024-10-14 15:54:12 +0800

    Update 3_Methodology.py

--- pages/3_Methodology.py
+++ pages/3_Methodology.py
@@ -1,8 +1,9 @@
 import streamlit as st
+from PIL import Image  # Import the Image module from the PIL library
 
 # Combined Welcome and About Us page
 st.title("Methodology")
-    
+
 # Load and display the local image from the "images" folder
 image = Image.open('images/methodology.png')  # Ensure the image is in the correct directory
 st.image(image, use_column_width=True)
